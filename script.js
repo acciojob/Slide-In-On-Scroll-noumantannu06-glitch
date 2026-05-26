437dd8417be879ffe941cf976408739463107f05
Md Nouman <noumantannu06@gmail.com>
2026-05-26 18:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 // Your JS code here.
+  const sliderImages = document.querySelectorAll(".slide-in");
+
+  function checkSlide() {
+    sliderImages.forEach((sliderImage) => {
+      const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
+      const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+      const isHalfShown = slideInAt > sliderImage.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+
+      if (isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add("active");
+      } else {
+        sliderImage.classList.remove("active");
+      }
+    });
+  }
+
+  function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+      const context = this;
+      const args = arguments;
+
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  window.addEventListener("scroll", debounce(checkSlide));
+
